e589083a450baf7c5b1d7d8d35ce80db36ae24a0
Mujtaba <mujtabakamil9@gmail.com>
2024-03-06 07:22:28 +0500

    authentication

--- vegetables/views.py
+++ vegetables/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from .models import *
 from django.shortcuts import redirect
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+
+from django.contrib import messages
 # Create your views here.
 
 
@@ -74,6 +77,36 @@ def login_page(request):
     return render(request,'login.html')
 
 def register(request):
+
+    if request.method == "POST":
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = User.objects.filter(username = username)
+
+        if user.exists():
+            messages.info(request, "Username already taken")
+
+            return redirect('/register/')
+        
+
+
+        user = User.objects.create(
+           first_name = first_name,
+           last_name = last_name,
+           username = username,
+          
+        )
+
+        user.set_password(password)
+        user.save()
+
+        messages.info(request, "Account created succesfully")
+
+        return redirect('/register/')
+
 
     return render(request , 'register.html')
 
